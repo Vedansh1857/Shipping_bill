ee202fdd426bf51eaf5da695c409753264a9b68f
Vedansh1857 <vedanshgupta606@gmail.com>
2024-07-30 14:54:14 +0530

    Updated

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(){
     // Get references to the fixed fields
-    const fixedField1 = document.getElementById('City');
+    const fixedField1 = document.getElementById('City1');
 
     // Add event listeners to the fixed fields
     fixedField1.addEventListener('blur', autoFillFields);
 
     function autoFillFields(){
         // Get references to the fields to be auto-filled
-        const autoField1 = document.getElementById('State');
-        const autoField2 = document.getElementById('Pin');
-        const autoField3 = document.getElementById('Country-of-Origin');
+        const autoField1 = document.getElementById('GSTN_ID');
+        const autoField2 = document.getElementById('Pin1');
+        // const autoField3 = document.getElementById('Country-of-Origin');
 
         // Check if both fixed fields are filled
         if (fixedField1.value) {
             if(fixedField1.value == "Ahemdabad")
             {
-                autoField1.value = 'Gujarat';
-                autoField2.value = '380015'
-                autoField3.value = 'India';
+                // autoField1.value = 'Gujarat';
+                autoField1.value = '24AAUDD97L1ZM'
+                autoField2.value = '380015';
             }
             else if(fixedField1.value == "Kanpur")
             {
-                autoField1.value = 'UP';
+                autoField1.value = 'A1B2C3D4E5';
                 autoField2.value = '208001'
-                autoField3.value = 'Bharat';
+                // autoField3.value = 'Bharat';
             }
             else
             {
